d2ffc090f12f24b68457cf2213d08b96c176c6cf
Aaditiya <pratapsinghaaditiya@gmail.com>
2026-06-18 16:53:43 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -31,6 +31,7 @@ import nltk
 nltk.download("stopwords")
 nltk.download("wordnet")
 nltk.download("omw-1.4")
+nltk.download("punkt_tab")
 
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
